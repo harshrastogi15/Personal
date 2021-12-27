1333e19d2b6f2e23373b48be25d3d716e9213d54
harshrastogi15 <hrastogi2002@gmail.com>
2021-12-27 14:52:04 +0530

    pic updated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 var ro = document.querySelector(':root');
 var mode= document.querySelector(".mode");
+var form= document.querySelector(".formdisplay");
+var contactform= document.getElementById("contactform");
 
 mode.addEventListener('click',()=>{
-    if(ro.style.getPropertyValue('--back-color')=='rgb(0, 0, 0)'){
+    if(ro.style.getPropertyValue('--back-color')=='rgb(255, 255, 255)'){
+        ro.style.setProperty('--main-color', 'rgb(255, 255, 255)');
+        ro.style.setProperty('--back-color', 'rgb(51, 52, 53)');
+    }
+    else{
         ro.style.setProperty('--main-color', 'rgb(83, 88, 92)');
         ro.style.setProperty('--back-color', 'rgb(255, 255, 255)');
     }
-    else{
-        ro.style.setProperty('--main-color', 'rgb(255, 255, 255)');
-        ro.style.setProperty('--back-color', 'rgb(0, 0, 0)');
-    }
 });
+
+form.addEventListener('click',()=>{
+    // console.log("heloo");
+    contactform.style.display="block";
+});
+
+function dnone(){
+    contactform.style.display="none";
+}
